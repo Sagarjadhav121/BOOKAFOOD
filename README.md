53979ba8a09163c71f7e87b32ae9268a47192a4f
Sagar Jadhav <jadhavsagar121@gmail.com>
2024-08-18 12:29:29 +0530

    DB connection + FE setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# BOOKAFOOD" 
+
+##BOOKAFOOD is a enterprise level food ordering platform
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,9 @@
 import express, {Request,Response} from 'express'; //Request,Response is type come from express
 import cors from 'cors';
 import "dotenv/config";
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=>console.log('connected to DB !'))
 
 const app=express();
 
